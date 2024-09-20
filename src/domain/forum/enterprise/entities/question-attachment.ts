6747ab40734e55ad23129b41bfea51520cc5e149
Diego Ferreira <diegoferreira1964@gmail.com>
2024-09-20 20:32:34 -0300

    feat: domain event structure

--- src/domain/forum/enterprise/entities/question-attachment.ts
+++ src/domain/forum/enterprise/entities/question-attachment.ts
@@ -1,5 +1,6 @@
 import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
 export interface QuestionAttachmentProps {
   questionId: UniqueEntityID
   attachmentId: UniqueEntityID
