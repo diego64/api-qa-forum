b5e6aaebc46d60364feb5f154b9d8030d4672b65
Diego Ferreira <diegoferreira1964@gmail.com>
2024-09-19 23:21:31 -0300

    feat: editing question with attachments

---
+++ src/domain/forum/application/repositories/question-attachments-repository.ts
@@ -0,0 +1,4 @@
+import { QuestionAttachment } from '../../enterprise/entities/question-attachment'
+export interface QuestionAttachmentsRepository {
+  findManyByQuestionId(questionId: string): Promise<QuestionAttachment[]>
+}
