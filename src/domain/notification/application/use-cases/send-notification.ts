cde8f0f58dec3c38b7f69eec593679ea27913a15
Diego Ferreira <diegoferreira1964@gmail.com>
2024-09-20 20:56:41 -0300

    feat: sending notification to subscriber

--- src/domain/notification/application/use-cases/send-notification.ts
+++ src/domain/notification/application/use-cases/send-notification.ts
@@ -3,19 +3,22 @@ import { Either, right } from '@/core/either'
 import { Notification } from '../../enterprise/entities/notification'
 import { NotificationsRepository } from '../repositories/notifications-repository'
 
-interface SendNotificationUseCaseRequest {
+export interface SendNotificationUseCaseRequest {
   recipientId: string
   title: string
   content: string
 }
-type SendNotificationUseCaseResponse = Either<
+
+export type SendNotificationUseCaseResponse = Either<
   null,
   {
     notification: Notification
   }
 >
+
 export class SendNotificationUseCase {
   constructor(private notificationsRepository: NotificationsRepository) {}
+
   async execute({
     recipientId,
     title,
@@ -26,7 +29,9 @@ export class SendNotificationUseCase {
       title,
       content,
     })
+
     await this.notificationsRepository.create(notification)
+
     return right({
       notification,
     })
