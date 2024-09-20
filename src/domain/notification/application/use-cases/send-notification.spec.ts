7386602ad9a65ad08e8a32589b5b1c6f617f4940
Diego Ferreira <diegoferreira1964@gmail.com>
2024-09-20 19:22:26 -0300

    chore: variable correction in test file

--- src/domain/notification/application/use-cases/send-notification.spec.ts
+++ src/domain/notification/application/use-cases/send-notification.spec.ts
@@ -11,8 +11,8 @@ describe('Send Notification', () => {
   it('should be able to send a notification', async () => {
     const result = await sut.execute({
       recipientId: '1',
-      title: 'Nova notificação',
-      content: 'Conteúdo da notificação',
+      title: 'New notification',
+      content: 'Notification content',
     })
     expect(result.isRight()).toBe(true)
     expect(inMemoryNotificationsRepository.items[0]).toEqual(
