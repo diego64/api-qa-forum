c8e213d529b269581335fa06bb194e665eaa3c7e
Diego Ferreira <diegoferreira1964@gmail.com>
2024-09-20 19:42:05 -0300

    feat: use case: read notifications

--- src/domain/notification/enterprise/entities/notification.ts
+++ src/domain/notification/enterprise/entities/notification.ts
@@ -1,13 +1,15 @@
 import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
-interface NotificationProps {
+
+export interface NotificationProps {
   recipientId: UniqueEntityID
   title: string
   content: string
   readAt?: Date
   createdAt: Date
 }
+
 export class Notification extends Entity<NotificationProps> {
   get recipientId() {
     return this.props.recipientId
@@ -29,6 +31,10 @@ export class Notification extends Entity<NotificationProps> {
     return this.props.createdAt
   }
 
+  read() {
+    this.props.readAt = new Date()
+  }
+
   static create(
     props: Optional<NotificationProps, 'createdAt'>,
     id?: UniqueEntityID,
@@ -40,6 +46,7 @@ export class Notification extends Entity<NotificationProps> {
       },
       id,
     )
+
     return notification
   }
 }
